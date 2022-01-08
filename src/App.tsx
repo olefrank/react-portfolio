6f6fada7d77d29290a563abeadb1023ac7904bc4
Ole Frank Jensen <oj@livahealth.com>
2022-01-08 10:21:11 +0100

    setup react-router + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
 import Increment from "./Increment";
 
 function App() {
   return (
-    <div className="App">
-      <Increment />
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link> | <Link to="increment">Increment</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="increment" element={<Increment />} />
+      </Routes>
     </div>
   );
 }
